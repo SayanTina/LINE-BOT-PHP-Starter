cb2dec33fbd0b069896625fde1e7deecfc54389c
CHAI <saya.tina671@gmail.com>
2017-10-20 19:26:04 +0700

    add config.php file

--- bot.php
+++ bot.php
@@ -1,10 +1,5 @@
 <?php
-$access_token = 'd60OWyK4zeSC9zmA9H/qSFQ/42MFixasGHIFcWtToOTgY+DhNly2tOksBsNCLTJIMs7C89ue1SUoxpY16K5GrTGo1kTXBTXZqPKdyTautrl2IYWFOP0sUAETOQgJkWdz70UT24PAA9jPHUPPUcq7nwdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
+include './config.php'
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+#######	Config ############
+line bot Message2Order
+*/
+
+$access_token = 'd60OWyK4zeSC9zmA9H/qSFQ/42MFixasGHIFcWtToOTgY+DhNly2tOksBsNCLTJIMs7C89ue1SUoxpY16K5GrTGo1kTXBTXZqPKdyTautrl2IYWFOP0sUAETOQgJkWdz70UT24PAA9jPHUPPUcq7nwdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+echo "include success";
+?>
