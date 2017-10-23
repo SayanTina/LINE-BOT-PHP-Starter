d5dd469fea48c0bfdef05cfd0939f778258fd802
CHAI <saya.tina671@gmail.com>
2017-10-24 00:40:44 +0700

    update database

--- database.php
+++ database.php
@@ -22,7 +22,7 @@ function AddOrder($messageOrder){
 
 function GetOrderAll(){
 	$connStrs = "host=ec2-54-204-41-80.compute-1.amazonaws.com port=5432 dbname=ddtqgibulmg329 user=xcouzcymallahy password=8110ad5d0c0f0f169502f0f61ce449a2704cbacc4f8d71b0aecf325701bca515";
-	$dbconn = pg_connect($connStr);
+	$dbconn = pg_connect($connStrs);
 	$reval = "0";
 	if($dbconn == null){
 		$reval = "failed";
