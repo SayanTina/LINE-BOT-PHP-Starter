f73d9b4adb95f09b71c5aa371c828a61db5cc194
CHAI <saya.tina671@gmail.com>
2017-10-24 20:44:57 +0700

    Add phpexcel

--- bot.php
+++ bot.php
@@ -18,6 +18,11 @@ if (!is_null($events['events'])) {
 					$splitdata = "get all\n";
 					$splitdata .= getAll();
 					break;
+				case "Excel":
+					$splitdata = "Excel file";
+					$data = GetOrderToExcel();
+					createExcel($data);
+					break;
 				default:
 					$splitdata = "another case\n";
 					$splitdata .= splitmessage($text);
@@ -61,4 +66,57 @@ function getAll(){
 	$result = GetOrderAll();
 	return $result;
 }
+function createExcel($data){
+	define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+	// Include PHPExcel 
+	require_once dirname(__FILE__) . './Classes/PHPExcel.php';
+
+	// Create new PHPExcel object
+	echo date('H:i:s') , " Create new PHPExcel object" , EOL;
+	$objPHPExcel = new PHPExcel();
+
+	// Set document properties
+	echo date('H:i:s') , " Set document properties" , EOL;
+	$objPHPExcel->getProperties()->setCreator("Sayan Tina")
+								 ->setLastModifiedBy("Sayan Tina")
+								 ->setTitle("PHPExcel Test Document")
+								 ->setSubject("PHPExcel Test Document")
+								 ->setDescription("Test document for PHPExcel, generated using PHP classes.")
+								 ->setKeywords("office PHPExcel php")
+								 ->setCategory("Test result file");
+
+	// Add some data
+	echo date('H:i:s') , " Add some data" , EOL;
+	$count = count($data);
+	$row = 1;
+	for($i = 0; $i < $count; $i++){
+		if($i%2 == 0){
+			$objPHPExcel->getActiveSheet()->setCellValue('A'.$row, $data[$i]);
+		}else{
+			$objPHPExcel->getActiveSheet()->setCellValue('B'.$row, $data[$i]);
+			$row++;
+		}
+	}
+
+   // Rename worksheet
+   echo date('H:i:s') , " Rename worksheet" , EOL;
+   $objPHPExcel->getActiveSheet()->setTitle('Simple');
+
+
+   // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+   $objPHPExcel->setActiveSheetIndex(0);
+
+
+   // Save Excel 2007 file
+   echo date('H:i:s') , " Write to Excel2007 format" , EOL;
+   $callStartTime = microtime(true);
+
+   $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+   $objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+   $callEndTime = microtime(true);
+   $callTime = $callEndTime - $callStartTime;
+
+}
 echo "OK";
+?>
