f2dbca8a4f59984c6560f6b4214e4f255195f1fc
CHAI <saya.tina671@gmail.com>
2017-10-24 22:05:58 +0700

    export_excel

--- Export_Excel.php
+++ Export_Excel.php
@@ -1,7 +1,7 @@
 <?php
 	include './database.php';
 	$data = GetOrderToExcel();
-	$filename = "export_order.xlsx"; 
+	$filename = "export_order.xls"; 
 	//header info for browser
 	header('Content-type: application/excel');
 	header('Content-Disposition: attachment; filename='.$filename);
