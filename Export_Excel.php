361938c4fb186e302876cfcf154e20fd8b8bb354
CHAI <saya.tina671@gmail.com>
2017-10-24 22:03:12 +0700

    export_excel

--- Export_Excel.php
+++ Export_Excel.php
@@ -1,32 +1,37 @@
 <?php
 	include './database.php';
 	$data = GetOrderToExcel();
-	$file_ending = "xls";
-	$filename = "export_order"; 
+	$filename = "export_order.xls"; 
 	//header info for browser
-	header("Content-Type: application/xls");    
-	header("Content-Disposition: attachment; filename=$filename.xls");  
-	header("Pragma: no-cache"); 
-	header("Expires: 0");
-	/*******Start of Formatting for Excel*******/   
-	//define separator (defines columns in excel & tabs in word)
-	$sep = "\t"; //tabbed character
-	//start of printing column names as names of MySQL fields
-	print("\n");    
-	//end of printing column names  
-	//start while loop to get data
-    $schema_insert = "";
-    for($i=0; $i<count($data);$i++)
-    {
-    	if($i%2 == 0){
-    		$schema_insert.= "$data[$i]"."\n";
-    	}else{
-    		$schema_insert.= "$data[$i]".$sep;
-    	}
-    }
-    $schema_insert = str_replace($sep."$", "", $schema_insert);
-    $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
-    $schema_insert .= "\t";
-    print(trim($schema_insert));
-    print "\n";
+	header('Content-type: application/excel');
+	header('Content-Disposition: attachment; filename='.$filename);
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?php echo $filename; ?></title>
+        <style>
+			table,th,td{
+				border: 1px solid black;
+				border-collapse:collapse;
+				text-align:center;
+			}
+		</style>
+    </head>
+    <body>
+    	<table>	
+			<?php
+			    for($i=0; $i<count($data);$i++)
+			    {
+			    	if($i%2 == 0){?>
+			    	<tr>
+			    		<td><?php echo "$data[$i]"; ?></td><?php
+			    	}else{?>
+			    		<td><?php echo "$data[$i]"; ?></td></tr><?php
+			    	}
+			    }
+			?>
+    	</table>
+    </body>
+</html>
